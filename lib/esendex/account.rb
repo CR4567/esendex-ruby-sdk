56134c20e809215998d5e55ba374fd905361056e
reichelt <reichelt@easierlife.de>
2014-12-09 18:13:22 +0100

    Added type, lang, retries parameters #Important for voice messages

--- lib/esendex/account.rb
+++ lib/esendex/account.rb
@@ -3,11 +3,11 @@ require 'nokogiri'
 module Esendex
   class Account
     attr_accessor :reference
-    
+
     def initialize(account_reference = Esendex.account_reference)
       @reference = account_reference
     end
-    
+
     def api_connection
       @api_connection ||= ApiConnection.new
     end
@@ -15,18 +15,18 @@ module Esendex
     def messages_remaining
       response = api_connection.get "/v1.0/accounts"
       doc = Nokogiri::XML(response.body)
-	  node = doc.at_xpath("//api:account[api:reference='#{@reference}']/api:messagesremaining", 'api' => Esendex::API_NAMESPACE)
-	  raise AccountReferenceError.new() if node.nil?
-	  node.content.to_i
+      node = doc.at_xpath("//api:account[api:reference='#{@reference}']/api:messagesremaining", 'api' => Esendex::API_NAMESPACE)
+      raise AccountReferenceError.new() if node.nil?
+      node.content.to_i
     end
 
     def send_message(args={})
       raise ArgumentError.new(":to required") unless args[:to]
       raise ArgumentError.new(":body required") unless args[:body]
 
-      send_messages([Message.new(args[:to], args[:body], args[:from])])
+      send_messages([Message.new(args[:to], args[:body], args[:from], args[:type], args[:lang], args[:retries])])
     end
-    
+
     def send_messages(messages)
       batch_submission = MessageBatchSubmission.new(@reference, messages)
       response = api_connection.post "/v1.0/messagedispatcher", batch_submission.to_s
--- lib/esendex/message.rb
+++ lib/esendex/message.rb
@@ -1,27 +1,34 @@
 require 'nokogiri'
 
 #  <message>
+#    <from>$FROM</from>
 #    <to>$TO</to>
 #    <body>$BODY</body>
+#    <type>$TYPE</type>
+#    <lang>$LANG</lang>
+#    <retries>$RETRIES</retries>
 #  </message>
 
 module Esendex
   class Message
-    attr_accessor :to, :body, :from
-    
-    def initialize(to, body, from=nil)
+    attr_accessor :to, :body, :from, :type, :lang, :retries
+
+    def initialize(to, body, from=nil, type='SMS', lang='en-GB', retries='3')
       @to = to
       @body = body
       @from = from
+      @type = type
+      @lang = lang
+      @retries = retries
     end
-    
+
     def xml_node
       doc = Nokogiri::XML('<message/>')
-                  
+
       to = Nokogiri::XML::Node.new 'to', doc
       to.content = @to
       doc.root.add_child(to)
-      
+
       body = Nokogiri::XML::Node.new 'body', doc
       body.content = @body
       doc.root.add_child(body)
@@ -31,7 +38,25 @@ module Esendex
         from.content = @from
         doc.root.add_child(from)
       end
-      
+
+      if @type == 'Voice'
+        type = Nokogiri::XML::Node.new 'type', doc
+        type.content = @type
+        doc.root.add_child(type)
+
+        if @lang
+          lang = Nokogiri::XML::Node.new 'lang', doc
+          lang.content = @lang
+          doc.root.add_child(lang)
+        end
+
+        if @retries
+          retries = Nokogiri::XML::Node.new 'retries', doc
+          retries.content = @retries
+          doc.root.add_child(retries)
+        end
+      end
+
       doc.root
     end
   end
